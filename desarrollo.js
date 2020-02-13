b56fdc32f68394a6d1c3a04226e57eaa12166d3c
robbfucu <robbfucu@gmail.com>
2020-02-13 15:20:47 -0600

    Update desarrollo.js
    corrected the file

--- desarrollo.js
+++ desarrollo.js
@@ -1,1 +1,6 @@
-var string = ["desarrollo"];
+var string = "ollorased";
+var reverseString = "";
+for(let i = string.length-1 ; i >= 0; i--){
+    reverseString += string[i]; 
+}
+console.log(reverseString);
