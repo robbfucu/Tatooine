9ceca2575f16eb5d0a40257c0c3347f9d15bb5dc
robbfucu <robbfucu@gmail.com>
2020-02-14 17:32:56 -0600

    Add files via upload
    decrypt.js

---
+++ decrypt.js
@@ -0,0 +1,29 @@
+ var mensaje = 'S23984203948023948234o34234345453y52345435345 345345345345345345u3453453453223452452345n324534545346654768780807896784865 535624643531453245234154g24366464565346r645634564576573a4573435735468n45684568 456d845e684658654867497465732456s246534a7567r456r74568465865865o86l7845685735743573457354735l67645865478564864584658658a45685484658456d456845684684563562453754o73573457437567345635688r';
+//(0,1) (21,22), (33,34) 45,46  65,65  87,88 121,122 146,147  161,162  177,178 191,192 200,201  204,205 208,209 233,234 240,241 245,246 249,250 264,265 267,268 293,294 320,321 335,336 360,361 384,385//
+var mensaje1 = mensaje.slice(0,1);
+var mensaje3 = mensaje.slice(21,22);
+var mensaje4 = mensaje.slice(33,34);
+var mensaje5 = mensaje.slice(45,46);
+var mensaje6 = mensaje.slice(64,65);
+var mensaje7 = mensaje.slice(87,88);
+var mensaje8 = mensaje.slice(121,122);
+var mensaje9 = mensaje.slice(146,147);
+var mensaje2 = mensaje.slice(161,162);
+var mensaje10 = mensaje.slice(177,178);
+var mensaje11 = mensaje.slice(191,192); 
+var mensaje12 = mensaje.slice(200,201);
+var mensaje13 = mensaje.slice(204,205);
+var mensaje14 = mensaje.slice(208,209);
+var mensaje15 = mensaje.slice(233,234);
+var mensaje16 = mensaje.slice(240,241);
+var mensaje17 = mensaje.slice(245,246);
+var mensaje18 = mensaje.slice(249,250);
+var mensaje19 = mensaje.slice(264,265);
+var mensaje20 = mensaje.slice(267,268);
+var mensaje21 = mensaje.slice(293,294);
+var mensaje22 = mensaje.slice(320,321);
+var mensaje23 = mensaje.slice(335,336);
+var mensaje24 = mensaje.slice(360,361);
+var mensaje25 = mensaje.slice(384,385);
+
+console.log(mensaje1+mensaje3+mensaje4+mensaje5+mensaje6+mensaje7+mensaje8+mensaje9+mensaje2+mensaje10+mensaje11+mensaje12+mensaje13+mensaje14+mensaje15+mensaje16+mensaje17+mensaje18+mensaje19+mensaje20+mensaje21+mensaje22+mensaje23+mensaje24+mensaje25);
